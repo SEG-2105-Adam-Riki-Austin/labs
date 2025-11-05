6ba16542bd994de41ffdf6d159e53ddac73acec9
Riki <rmcal024@uottawa.ca>
2025-11-05 18:19:07 -0500

    Component id naming

--- lab-5/Lab5ProfileManager/app/src/main/java/com/example/lab5profilemanager/MainActivity.java
+++ lab-5/Lab5ProfileManager/app/src/main/java/com/example/lab5profilemanager/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.lab5profilemanager;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonGoogleMaps;
+    EditText editTeamName, editTeamPostalAddress;
+    TextView textHeading, textTeamName, textTeamPostalAddress;
+    ImageView imageTeamLogo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        buttonGoogleMaps = findViewById(R.id.buttonGoogleMaps);
+        editTeamName = findViewById(R.id.editTeamName);
+        editTeamPostalAddress = findViewById(R.id.editTeamPostalAddress);
+        textTeamName = findViewById(R.id.textTeamName);
+        textTeamPostalAddress = findViewById(R.id.textTeamPostalAddress);
+        textHeading = findViewById(R.id.textHeading);
+        imageTeamLogo = findViewById(R.id.imageTeamLogo);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- lab-5/Lab5ProfileManager/app/src/main/java/com/example/lab5profilemanager/ProfileActivity.java
+++ lab-5/Lab5ProfileManager/app/src/main/java/com/example/lab5profilemanager/ProfileActivity.java
@@ -10,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ProfileActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
