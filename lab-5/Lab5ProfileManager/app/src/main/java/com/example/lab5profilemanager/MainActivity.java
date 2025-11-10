af4a9bfeef952239aa3fb9d7389d04a18ebb1611
Riki <rmcal024@uottawa.ca>
2025-11-09 19:34:46 -0500

    Avatar Intent

--- lab-5/Lab5ProfileManager/app/src/main/java/com/example/lab5profilemanager/MainActivity.java
+++ lab-5/Lab5ProfileManager/app/src/main/java/com/example/lab5profilemanager/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.lab5profilemanager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+// Import UI components
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -14,17 +18,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Declare views
     Button buttonGoogleMaps;
     EditText editTeamName, editTeamPostalAddress;
     TextView textHeading, textTeamName, textTeamPostalAddress;
     ImageView imageTeamLogo;
 
+    // Called when the activity is first created.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        // Finds the views in the XML layout with the IDs provided
         buttonGoogleMaps = findViewById(R.id.buttonGoogleMaps);
         editTeamName = findViewById(R.id.editTeamName);
         editTeamPostalAddress = findViewById(R.id.editTeamPostalAddress);
@@ -39,4 +46,10 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void intentProfileActivity(View view) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
 }
