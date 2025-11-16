881b2d81a1dd4b4219d9822427af51df965ca5fe
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-11-15 21:35:43 -0500

    feat(lab-6): Miscellaneous improvements
    Closing requests, reduced repetition, better comments.

--- lab-6/main.go
+++ lab-6/main.go
@@ -16,6 +16,8 @@ type FetchResult struct {
 func worker(id int, jobs <-chan string, results chan<- FetchResult) {
 	for page := range jobs {
 		fmt.Printf("Worker %d processing job %s\n", id, page)
+
+		// Open request to remote
 		resp, err := http.Get(page)
 		if err != nil {
 			results <- FetchResult{
@@ -26,22 +28,30 @@ func worker(id int, jobs <-chan string, results chan<- FetchResult) {
 			}
 			continue
 		}
+
+		requestUrl := resp.Request.URL.String()
+
 		// Get response size
-		size, err := io.Copy(io.Discard, resp.Body)
+		body := resp.Body
+		size, err := io.Copy(io.Discard, body)
+		body.Close()
+
 		if err != nil {
 			results <- FetchResult{
-				URL:        resp.Request.URL.String(),
+				URL:        requestUrl,
 				StatusCode: resp.StatusCode,
 				Size:       -1,
 				Error:      err,
 			}
 			continue
 		}
+
+		// Send successful result to channel
 		results <- FetchResult{
-			URL:        resp.Request.URL.String(),
+			URL:        requestUrl,
 			StatusCode: resp.StatusCode,
 			Size:       int(size),
-			Error:      err,
+			Error:      nil,
 		}
 	}
 }
@@ -57,6 +67,7 @@ func main() {
 
 	numWorkers := 3
 
+	// Set up channels
 	jobs := make(chan string, len(urls))
 	results := make(chan FetchResult, len(urls))
 
@@ -65,12 +76,13 @@ func main() {
 		go worker(w+1, jobs, results)
 	}
 
-	// Send jobs
+	// Send URLs to fetch
 	for i := range urls {
 		jobs <- urls[i]
 	}
 	close(jobs)
 
+	// Print results as they arrive
 	for range urls {
 		fmt.Println("Result:", <-results)
 	}
